1d3bbda607432d6b42274d93bdc9cd028a8ecedb
waldemar.parzonka@gmail.com <waldemar.parzonka@gmail.com>
2024-06-26 16:25:56 +0100

    fixup! first stab

---
+++ urkel.go
@@ -0,0 +1,370 @@
+package veracity
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math"
+
+	"github.com/datatrails/go-datatrails-merklelog/massifs"
+	"github.com/urfave/cli/v2"
+)
+
+type Node struct {
+	prefix      []byte
+	pdepth      int
+	terminating bool
+	Left        *Node
+	Right       *Node
+	Value       string
+	Hash        string
+}
+
+// isInternal returns true if a node is not a leaf
+func (n *Node) isInternal() bool {
+	return n.Value == "" && !n.terminating
+}
+
+// isTerminating returns true if a node is terminating node
+// with a zero hash
+func (n *Node) isTerminating() bool {
+	return n.terminating
+}
+
+type BinaryUrkelTrie struct {
+	Root *Node
+}
+
+func NewBinaryUrkelTrie() *BinaryUrkelTrie {
+	return &BinaryUrkelTrie{}
+}
+
+func newLeafFromLeaf(leaf *Node) *Node {
+	return &Node{
+		Value:       leaf.Value,
+		prefix:      leaf.prefix,
+		pdepth:      leaf.pdepth,
+		Hash:        leaf.Hash,
+		terminating: leaf.terminating,
+	}
+}
+
+func hash(data string) string {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func (t *BinaryUrkelTrie) Insert(key []byte, value string) {
+	nn := &Node{
+		prefix: key,
+		// this is new leaf - so set the significant bits to all of them
+		pdepth: len(key) * 8,
+		Value:  value,
+		Hash:   hash(value),
+	}
+	t.Root = t.insert(t.Root, nn, 0)
+}
+
+// prefixForDepth returns a new prefix with depth bits from the prefix
+// with last significant bit set to lastSignigicant if one is defined
+func prefixForDepth(depth int, prefix []byte, lastSignificant *int) []byte {
+	b := make([]byte, len(prefix))
+	for i, bb := range prefix {
+		if i == depth/8 {
+			break
+		}
+		b[i] = bb
+	}
+
+	if depth/8 == len(prefix) {
+		return b
+	}
+
+	b[depth/8] = (prefix[depth/8] >> (depth % 8)) << (depth % 8)
+
+	if lastSignificant != nil && *lastSignificant == 1 {
+		mask := (byte(0xff) >> (depth % 8))
+		b[depth/8] = b[depth/8] | mask
+	} else if lastSignificant != nil {
+		mask := (byte(0xff) >> (depth % 8)) << (depth % 8)
+		b[depth/8] = b[depth/8] & mask
+	}
+
+	return b
+}
+
+func (t *BinaryUrkelTrie) insert(node *Node, new *Node, depth int) *Node {
+
+	// if we have an nil noce we just replace
+	// with new node (this will only be a case for first leaf)
+	if node == nil {
+		return new
+	}
+
+	// if we're inserting a leaf and our node is a terminating node
+	// we just replace it
+	if !new.isInternal() && !new.isTerminating() && node.isTerminating() {
+		return new
+	}
+
+	// find out on how many bits we match the current node
+	// up to the depth of the node
+	tdepth := min(commonPrefix(node.prefix, new.prefix), node.pdepth)
+
+	// which bit to check for left?right
+	prefixNext := tdepth
+	if node.isTerminating() || node.isInternal() || new.isInternal() || (node == t.Root && node.isInternal()) {
+		// if we're not dealing with leafs we want to clamp to current depth
+		prefixNext = min(tdepth, depth)
+	}
+
+	// check the bit after the common part
+	// zeroes go to the left
+	left := !bit(prefixNext, new.prefix)
+
+	one := 1
+	zero := 0
+
+	if left { //new.prefix[targetDepth] == '0' {
+		if node.isInternal() {
+			// we hit internal node - we must go deeper
+			node.Left = t.insert(node.Left, new, depth+1)
+		} else if node.isTerminating() {
+			// if we're inserting an internal node
+			// add the depth if aligned with our target depth
+			// we just replace the terminating node
+			if new.isInternal() && depth == new.pdepth {
+				return new
+			}
+
+			if new.isInternal() {
+				// if the deptht is different and we're insetgin internal node
+				// we create another terminating node to the left
+				node.Left = terminatingNode(prefixForDepth(depth+1, new.prefix, &zero), depth+1)
+			}
+
+			// make current node not terminating any more
+			// and insert our new node to the left then add terminating one on the right
+			node.terminating = false
+			node.Left = t.insert(node.Left, new, depth+1)
+			node.Right = terminatingNode(prefixForDepth(depth+1, new.prefix, &one), depth+1)
+		} else {
+			// we have two leafs
+			// we need to create an internal node with a target depth
+			// of common prefix between our leafs
+			newNode := &Node{
+				prefix: prefixForDepth(tdepth, new.prefix, nil),
+				pdepth: tdepth,
+				Left:   new,
+				Right:  newLeafFromLeaf(node),
+				Hash:   hash(new.Hash + node.Hash),
+			}
+			// make current node into an internal node with two terminating leafs
+			nodeToDoubleTerminating(node, new.prefix, depth)
+			// and if the depth does not match the target depth insert our new internal node
+			if depth != tdepth {
+				return t.insert(node, newNode, depth)
+			}
+
+			// otherwise just return new node
+			return newNode
+		}
+	} else {
+		// repeat for right side scenario
+		if node.isInternal() {
+			node.Right = t.insert(node.Right, new, depth+1)
+		} else if node.isTerminating() {
+			if new.isInternal() && depth == new.pdepth {
+				return new
+			}
+			if new.isInternal() {
+				node.Right = terminatingNode(prefixForDepth(depth+1, new.prefix, &one), depth+1)
+			}
+			node.terminating = false
+			node.Right = t.insert(node.Right, new, depth+1)
+			node.Left = terminatingNode(prefixForDepth(depth+1, new.prefix, &zero), depth+1)
+		} else {
+			newNode := &Node{
+				prefix: prefixForDepth(tdepth, new.prefix, nil), //node.prefix[:targetDepth],
+				pdepth: tdepth,
+				Left:   newLeafFromLeaf(node),
+				Right:  new,
+				Hash:   hash(node.Hash + new.Hash),
+			}
+
+			nodeToDoubleTerminating(node, new.prefix, depth)
+			if depth != tdepth {
+				return t.insert(node, newNode, depth)
+			}
+
+			return newNode
+		}
+	}
+
+	// if it's terminating node just return as we don't have anything below it
+	if node.isTerminating() {
+		return node
+	}
+
+	// on the way back up the tree
+	// we need to update the hashes
+	// we combine left + right
+	nodeHash := node.Value
+	if node.Left != nil {
+		nodeHash += node.Left.Hash
+	}
+	if node.Right != nil {
+		nodeHash += node.Right.Hash
+	}
+	node.Hash = hash(nodeHash)
+
+	// set the depth to current
+	node.pdepth = depth
+	return node
+}
+
+// turn node into a double terminated node
+//
+//	n     ->     n
+//	            / \
+//	           x   x
+func nodeToDoubleTerminating(node *Node, prefix []byte, depth int) {
+	one := 1
+	zero := 0
+	node.terminating = false
+	node.prefix = prefixForDepth(depth, prefix, nil)
+	node.pdepth = depth
+	node.Value = ""
+	node.Hash = ""
+	node.Left = terminatingNode(prefixForDepth(depth+1, prefix, &zero), depth+1)
+	node.Right = terminatingNode(prefixForDepth(depth+1, prefix, &one), depth+1)
+}
+
+// return new terminating node with a prefix and depth target
+// and hash of all 0
+func terminatingNode(prefix []byte, depth int) *Node {
+	return &Node{
+		prefix:      prefix,
+		pdepth:      depth,
+		terminating: true,
+		Hash:        "0000000000000000000000000000000000000000000000000000000000000000",
+	}
+}
+
+// GetPath gets a path for a key
+// return list of hex strings for all nodes leading up
+// to leaf with specified key - and return value of that leaf
+// as well as true if the key was found or false if we hit a terminating node
+// or other leaf early
+func (t *BinaryUrkelTrie) GetPath(key []byte) ([]string, string, bool) {
+	path := []string{}
+	node := t.Root
+	for i := 0; i <= len(key)*8; i++ {
+
+		if node == nil {
+			return path, "", false
+		}
+
+		path = append(path, node.Hash)
+		// this is leaf value we have to break out
+		if node.Value != "" {
+			break
+		}
+
+		if node.isTerminating() {
+			return path, "", false
+		}
+
+		if bit(i, key) {
+			node = node.Right
+		} else {
+			node = node.Left
+		}
+	}
+
+	if !bytes.Equal(node.prefix, key) {
+		return path, node.Value, false
+	}
+
+	return path, node.Value, true
+}
+
+// bit returns true if bit at positing is 1, false otherwise
+func bit(pos int, value []byte) bool {
+	b := value[(pos)/8]
+	r := (pos) % 8
+	m := 1 << (7 - r)
+	return b&byte(m) != 0
+}
+
+// returns lengts of common prefix of two byte arrays
+func commonPrefix(node, value []byte) int {
+	commonBytes := 0
+	for i, b := range node {
+		if len(value) <= i {
+			break
+		}
+		bb := b ^ value[i]
+		if bb == 0x0 {
+			commonBytes += 1
+			continue
+		}
+
+		break
+	}
+
+	if commonBytes == len(node) || commonBytes == len(value) {
+		return commonBytes * 8
+	}
+
+	nb := node[commonBytes]
+	vb := value[commonBytes]
+
+	commonBits := 8
+	for nb != vb {
+		nb = nb >> 1
+		vb = vb >> 1
+		commonBits -= 1
+	}
+
+	return commonBytes*8 + commonBits
+}
+
+// NewUrkelCommand implements a sub command which construcst an urkel trie
+func NewUrkelCommand() *cli.Command {
+	return &cli.Command{Name: "urkel",
+		Usage: "construct an urkel from massif",
+		Flags: []cli.Flag{
+			&cli.Int64Flag{
+				Name: "massif", Aliases: []string{"m"},
+			},
+			&cli.StringFlag{
+				Name: "value", Aliases: []string{"v"},
+			},
+			&cli.BoolFlag{Name: "massif-relative", Aliases: []string{"r"}},
+		},
+		Action: func(cCtx *cli.Context) error {
+			var err error
+			cmd := &CmdCtx{}
+
+			if err = cfgMassif(cmd, cCtx); err != nil {
+				return err
+			}
+
+			start := cmd.massif.LogStart()
+			count := cmd.massif.Count()
+			urkel := NewBinaryUrkelTrie()
+			for i := uint64(0); i < count; i++ {
+				val := cmd.massif.Data[start+i*massifs.ValueBytes : start+i*massifs.ValueBytes+massifs.ValueBytes]
+				key := cmd.massif.Data[start+i*massifs.TrieKeyBytes : start+i*massifs.TrieKeyBytes+massifs.TrieKeyBytes]
+				urkel.Insert(key, hex.EncodeToString(val))
+
+				fmt.Printf("%d: %s %d %d\n", i+cmd.massif.Start.FirstIndex, hex.EncodeToString(key), len(key), math.Ilogb(float64(len(key))*8))
+
+			}
+			return nil
+		},
+	}
+}
--- urkel_test.go
+++ urkel_test.go
@@ -9,126 +9,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestCommonPrefix(t *testing.T) {
+func TestCommonPrefix(t *testing.T) {
 
-// 	logger.New("TestVerifyList")
-// 	defer logger.OnExit()
-// 	url := os.Getenv("TEST_INTEGRATION_FORESTRIE_BLOBSTORE_URL")
-// 	logger.Sugar.Infof("url: '%s'", url)
+	logger.New("TestVerifyList")
+	defer logger.OnExit()
+	url := os.Getenv("TEST_INTEGRATION_FORESTRIE_BLOBSTORE_URL")
+	logger.Sugar.Infof("url: '%s'", url)
 
-// 	tests := []struct {
-// 		name    string
-// 		node    []byte
-// 		value   []byte
-// 		outcome int
-// 	}{
-// 		{
-// 			name:    "same",
-// 			node:    []byte{0x00, 0x01, 0x02, 0x00},
-// 			value:   []byte{0x00, 0x01, 0x02, 0x00},
-// 			outcome: 32,
-// 		},
-// 		{
-// 			name:    "simple offset",
-// 			node:    []byte{0x00, 0x01, 0x02, 0x00},
-// 			value:   []byte{0x00, 0x01, 0x02, 0x01},
-// 			outcome: 31,
-// 		},
-// 		{
-// 			name:    "is on the right",
-// 			node:    []byte{0x00, 0x01, 0x02, 0x02},
-// 			value:   []byte{0x00, 0x01, 0x02, 0x01},
-// 			outcome: 30,
-// 		},
-// 		{
-// 			name:    "has gap",
-// 			node:    []byte{0x00, 0x01, 0x02, 0x00},
-// 			value:   []byte{0x00, 0x01, 0x02, 0x02},
-// 			outcome: 30,
-// 		},
-// 		{
-// 			name:    "wrap around",
-// 			node:    []byte{0x00, 0x01, 0x02, 0xff},
-// 			value:   []byte{0xff, 0x01, 0x03, 0x00},
-// 			outcome: 0,
-// 		},
-// 		{
-// 			name:    "wrap around",
-// 			node:    []byte{0x01, 0x01, 0x02, 0xff},
-// 			value:   []byte{0x05, 0x01, 0x03, 0x00},
-// 			outcome: 0,
-// 		},
-// 	}
+	tests := []struct {
+		name    string
+		node    []byte
+		value   []byte
+		outcome int
+	}{
+		{
+			name:    "same",
+			node:    []byte{0x00, 0x01, 0x02, 0x00},
+			value:   []byte{0x00, 0x01, 0x02, 0x00},
+			outcome: 32,
+		},
+		{
+			name:    "simple offset",
+			node:    []byte{0x00, 0x01, 0x02, 0x00},
+			value:   []byte{0x00, 0x01, 0x02, 0x01},
+			outcome: 31,
+		},
+		{
+			name:    "is on the right",
+			node:    []byte{0x00, 0x01, 0x02, 0x02},
+			value:   []byte{0x00, 0x01, 0x02, 0x01},
+			outcome: 30,
+		},
+		{
+			name:    "has gap",
+			node:    []byte{0x00, 0x01, 0x02, 0x00},
+			value:   []byte{0x00, 0x01, 0x02, 0x02},
+			outcome: 30,
+		},
+		{
+			name:    "wrap around",
+			node:    []byte{0x00, 0x01, 0x02, 0xff},
+			value:   []byte{0xff, 0x01, 0x03, 0x00},
+			outcome: 0,
+		},
+		{
+			name:    "wrap around",
+			node:    []byte{0x01, 0x01, 0x02, 0xff},
+			value:   []byte{0x05, 0x01, 0x03, 0x00},
+			outcome: 5,
+		},
+	}
 
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			r := commonPrefix(tc.node, tc.value)
-// 			assert.Equal(t, tc.outcome, r)
-// 			fmt.Printf("vvvv %v\n", tc.value)
-// 		})
-// 	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			r := commonPrefix(tc.node, tc.value)
+			assert.Equal(t, tc.outcome, r)
+			fmt.Printf("vvvv %v\n", tc.value)
+		})
+	}
 
-// }
+}
 
-// func TestCommonDepth(t *testing.T) {
-
-// 	logger.New("TestVerifyList")
-// 	defer logger.OnExit()
-// 	url := os.Getenv("TEST_INTEGRATION_FORESTRIE_BLOBSTORE_URL")
-// 	logger.Sugar.Infof("url: '%s'", url)
-
-// 	tests := []struct {
-// 		name    string
-// 		value   int
-// 		length  int
-// 		outcome int
-// 	}{
-// 		{
-// 			name:    "same",
-// 			value:   5,
-// 			length:  4,
-// 			outcome: 88,
-// 		},
-// 		{
-// 			name:    "simple offset",
-// 			value:   31,
-// 			length:  32,
-// 			outcome: 5,
-// 		},
-// 		{
-// 			name:    "is on the right",
-// 			value:   30,
-// 			length:  32,
-// 			outcome: 5,
-// 		},
-// 		{
-// 			name:    "has gap",
-// 			value:   5,
-// 			length:  32,
-// 			outcome: 3,
-// 		},
-// 		{
-// 			name:    "wrap around",
-// 			value:   255,
-// 			length:  256,
-// 			outcome: 8,
-// 		},
-// 		{
-// 			name:    "wrap around",
-// 			value:   256,
-// 			length:  256,
-// 			outcome: 9,
-// 		},
-// 	}
-
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			r := commonDepth(tc.value, tc.length)
-// 			assert.Equal(t, tc.outcome, r)
-// 		})
-// 	}
-
-// }
 func TestBit(t *testing.T) {
 
 	logger.New("TestVerifyList")
@@ -143,21 +84,39 @@ func TestBit(t *testing.T) {
 		outcome bool
 	}{
 		{
-			name:    "simple",
+			name:    "miss",
 			value:   []byte{0b10},
 			pos:     1,
+			outcome: false,
+		},
+		{
+			name:    "miss two bytes",
+			value:   []byte{0b10, 0b11},
+			pos:     8,
+			outcome: false,
+		},
+		{
+			name:    "hit two bytes",
+			value:   []byte{0b10, 0b11},
+			pos:     14,
 			outcome: true,
 		},
 		{
-			name:    "simple",
+			name:    "miss two bytes 2",
 			value:   []byte{0b10, 0b11},
-			pos:     8,
+			pos:     13,
+			outcome: false,
+		},
+		{
+			name:    "hit two bytes 2",
+			value:   []byte{0b10, 0b11},
+			pos:     15,
 			outcome: true,
 		},
 		{
 			name:    "hit on first",
 			value:   []byte{0b1},
-			pos:     0,
+			pos:     7,
 			outcome: true,
 		},
 		{
@@ -169,19 +128,19 @@ func TestBit(t *testing.T) {
 		{
 			name:    "miss",
 			value:   []byte{0b1000000},
-			pos:     7,
+			pos:     0,
 			outcome: false,
 		},
 		{
 			name:    "miss 2",
 			value:   []byte{0b1000000},
-			pos:     5,
+			pos:     2,
 			outcome: false,
 		},
 		{
 			name:    "hit 2",
 			value:   []byte{0b1000000},
-			pos:     6,
+			pos:     1,
 			outcome: true,
 		},
 	}
@@ -195,7 +154,7 @@ func TestBit(t *testing.T) {
 
 }
 
-func TestAddToUrkle(t *testing.T) {
+func TestAddToUrkel(t *testing.T) {
 
 	logger.New("TestVerifyList")
 	defer logger.OnExit()
@@ -398,7 +357,6 @@ func TestAddToUrkle(t *testing.T) {
 			urkel := NewBinaryUrkelTrie()
 			for v, b := range tc.data {
 				urkel.Insert(b, v)
-				//urkel = AddToUrkle(CreateLeafithKeyValue(b, b), urkel, 0, 0)
 			}
 			fmt.Printf("\n%v\n", urkel)
 
